e2288174b818af646fe9e45fd27df74f06cdd620
BlueThunderD <danag@DESKTOP-9KN3MO2>
2020-01-16 22:38:01 +0200

    added iamges to catalog

--- client/Controllers/ItemListViewCell.java
+++ client/Controllers/ItemListViewCell.java
@@ -9,12 +9,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 public class ItemListViewCell extends ListCell<CatalogItem>{
 	@FXML private Label Title;
 	@FXML private Label Description;
 	@FXML private Label Color;
+	@FXML private ImageView ivIm1;
 	
 	@FXML private AnchorPane pane;
 	
@@ -46,7 +49,7 @@ public class ItemListViewCell extends ListCell<CatalogItem>{
             Title.setText(String.valueOf(item.getName()));
             Description.setText(String.valueOf(item.getDescription()));
             Color.setText(String.valueOf(item.getColor()));
-            
+            ivIm1.setImage(new Image(item.getImagePath()));
 
             setText(null);
             setGraphic(pane);

1b9538eb6d181253e8ae1f2d26b5951d66ef57fe
shirbuch <shirbuchner@yahoo.com>
2020-01-27 23:22:04 +0200

    @override in employee

--- common/Employee.java
+++ common/Employee.java
@@ -5,4 +5,9 @@ public class Employee extends User {
 	public Employee(String username, String password, int user_id_, int store) {
 		super(username, password, user_id_, store);
 	}
+	
+	@Override
+	public String getDetails() {
+		return "User: " + username + "\nPassword: " + password + "\nUser ID: "+ user_id + "\nStore: "+ store;
+	}
 }

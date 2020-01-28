4d14ab4e1e90df060e5c2b1fad940c3a76258fdd
shirbuch <shirbuchner@yahoo.com>
2020-01-28 14:08:02 +0200

    signup input check

--- client/Controllers/SignUpController.java
+++ client/Controllers/SignUpController.java
@@ -81,14 +81,14 @@ public class SignUpController {
     
     @FXML
     void signUp(ActionEvent event) {
-    	String user_name = nameText.getText();
-    	String I_D = IDText.getText();
-    	String passwd = passText.getText();
-    	String passValidate = oassAgainText.getText();
-    	String cardNumber = CardNumberText.getText();
+    	String user_name = removeSpaces(nameText.getText());
+    	String I_D = removeSpaces(IDText.getText());
+    	String passwd = removeSpaces(passText.getText());
+    	String passValidate = removeSpaces(oassAgainText.getText());
+    	String cardNumber = removeNonNumbers(removeSpaces(CardNumberText.getText()));
     	int pay_method = PayingMethod.pay_per_order;
-    	String phoneNumber = PhoneNumberText.getText();
-    	String email = EmailText.getText();
+    	String phoneNumber = removeNonNumbers(removeSpaces(PhoneNumberText.getText()));
+    	String email = removeSpaces(EmailText.getText());
     	System.out.println(storeselector.getText().substring(0,1));
     	int store = Integer.parseInt(storeselector.getText().substring(0,1));
     	
@@ -100,7 +100,8 @@ public class SignUpController {
     		pay_method = PayingMethod.annual_subscription;
     	
     	try {
-    		checkInput(passwd, passValidate);
+    		checkInput(passwd, passValidate, user_name, I_D, cardNumber, 
+    	    		phoneNumber, email);
     	} catch(IOException e) {
     		new Alert(Alert.AlertType.ERROR, e.getMessage()).showAndWait();
     		return;
@@ -140,7 +141,7 @@ public class SignUpController {
     void updateStoresList() {
     	EventHandler<ActionEvent> event1 = new EventHandler<ActionEvent>() { 
             public void handle(ActionEvent e) 
-            { 
+            {
             	storeselector.setText(((MenuItem)e.getSource()).getText() + ""); 
             } 
         }; 
@@ -164,19 +165,66 @@ public class SignUpController {
     }
     
     
-    private void checkInput(String pass,  String secondPass) throws IOException {
+    private void checkInput(String passwd, String passValidate, String user_name, String I_D, String cardNumber, 
+    		String phoneNumber, String email) throws IOException {
     	String error_message = "";
-    	if(!(pass.length()>1))
-    		error_message += "Your password is short\n";
-    	if(!pass.equals(secondPass))
-    		error_message += "Your two enterd passwords are not equal\n";
+    	
+    	if(user_name == null)
+    		error_message += "No usename\n";
+    	if(passwd == null)
+    		error_message += "No Password\n";
+    	if(I_D == null)
+    		error_message += "No ID\n";
+    	if(cardNumber == null)
+    		error_message += "No card number\n";
+    	
+    	if(!(passwd.length()>6))
+    		error_message += "Your password is too short\n";
+    	if(!passwd.equals(passValidate))
+    		error_message += "Your enterd passwords are not equal\n";
   
+    	if(I_D.length()!=9)
+    		error_message += "Your ID length isn't 9\n";
+    	
+    	if(cardNumber.length()<11 || cardNumber.length()>19)
+    		error_message += "Your card number length isn't right\n";
+    	
+    	if(phoneNumber.length()!=10 || phoneNumber.length()!=12) // Israeli
+    		error_message += "Your phone number length isn't right\n";
+    			
+    	if(!validEmail(email))
+    		error_message += "Your email isn't right\n";
+
+    	
     	if(error_message.length() > 0)
     		throw(new IOException(error_message));
-    		
     }
 
+    public String removeSpaces(String str){
+		String temp = "";
+		for(int i = str.length()-1; i > 0; --i) {
+			if(str.charAt(i) != ' ')
+				temp += str.charAt(i);
+			else
+				i = str.length()+1;
+		}
+		String temp2 = "";
+		for(int i = 0; i < temp.length(); ++i) 
+			temp2 += temp.charAt(temp.length()-1-i);
+    	return temp2;
+    }
     
+    public String removeNonNumbers(String str){
+    	String temp = "";
+		for(int i = 0; i < str.length(); ++i) 
+			if(str.charAt(i) < '0' && str.charAt(i) > '9')
+				temp += str.charAt(i);
+		return temp;
+    }
     
+    boolean validEmail(String email) {
+        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        return email.matches(regex);
+     }
     
 }
